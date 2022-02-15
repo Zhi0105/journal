e8877e59504af9fe435cdf2bf196ebecc551c072
Argie Barcena <barcenaargie@gmail.com>
2022-02-15 23:58:25 +0800

    Displaying due dates tasks on greeting page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,14 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+      @id = current_user.id
+      @tasklist = Task.find_by_sql(
+        " select categories.name as category_name, tasks.name as task, tasks.due_date as due
+        from tasks left join categories on categories.id = tasks.category_id
+        left join users on users.id = categories.user_id
+        where categories.user_id = #{@id}
+      ")
+      
   end
+
 end
